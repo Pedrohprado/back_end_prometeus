cce924d1a0a9a7b441d5d0886b263e7d432c2948
Pedro Henrique Prado <pedroh.prado@outlook.com>
2024-07-31 11:37:08 -0300

    add initializing webscoket

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { apiRouter } = require('./router/routes');
+const { initializeWebSocket } = require('./services/websocketService');
+const { runAll } = require('./services/sendWebsocketInfo');
 
 const app = express();
 
@@ -14,6 +16,15 @@ app.use((req, res) => {
   res.status(404).json({ error: 'endpoint not fould' });
 });
 
-const teste = app.listen(2222, process.env.IP_SERVER, () => {
+const teste = app.listen(8080, process.env.IP_SERVER, () => {
   console.log('running!');
 });
+
+initializeWebSocket(teste);
+
+// return this fucntion when prometeus have datas
+setInterval(() => {
+  if (new Date().getHours() >= 5 && new Date().getHours() <= 16) {
+    runAll();
+  }
+}, 2000);
